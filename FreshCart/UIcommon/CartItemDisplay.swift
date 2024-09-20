584fb68c51864ee9e667cd35ae18faffe36dcd02
cao đức tín <102477829+caoductin@users.noreply.github.com>
2024-09-20 20:32:19 +0700

    add item to cart

---
+++ FreshCart/UIcommon/CartItemDisplay.swift
@@ -0,0 +1,18 @@
+//
+//  CartItemDisplay.swift
+//  FreshCart
+//
+//  Created by cao duc tin  on 19/9/24.
+//
+
+import SwiftUI
+
+struct CartItemDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartItemDisplay()
+}
